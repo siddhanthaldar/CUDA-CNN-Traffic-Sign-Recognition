27ddf27aedc449e2f3f782e1fce294b29a1a6bc5
harsh-99 <harshmaheshwari135@gmail.com>
2019-04-09 04:29:06 +0530

    adding new dataset of 15 class

--- create_dataset.py
+++ create_dataset.py
@@ -1,32 +1,48 @@
 import numpy as np
 import os
 import cv2
-
-file = open("dataset.txt", 'w')
+# 9->0, 35->1, 13->2, 4->3, 38->4, 11->5, 12->6, 25->7, 7->8, 1->9, 3->10, 10->11, 8->12, 2->13, 5->14
+file = open("new_dataset.txt", 'w')
 dataset = []
-
+k = 0
+c = [1,2,3,4,5,7,8,9,10,11,12,13,25,35,38]
 classes = os.listdir("GTSRB/Final_Training/Images/")
+print(classes)
+
+d = np.zeros((43,43))
+
 for class_no, folder in enumerate(classes):
-	print class_no
-	images = os.listdir('GTSRB/Final_Training/Images/' + folder)
-	for image in images:
-		name = image[9:11]
-		if name != '29':
-			continue
-		img = cv2.imread('GTSRB/Final_Training/Images/' + folder + '/' + image, 1)
-		img = cv2.resize(img, (32, 32))
-		# print img.shape
-		data = [int(folder)]
-		for i in range(0, 3):
-			data += list(img[:, :, i].reshape(1, -1).squeeze())
-		dataset.append(data)
+	print (folder)
+	for j in range(len(c)):
+		if(c[j] == int(folder)):
+			images = os.listdir('GTSRB/Final_Training/Images/' + folder)
+			for image in images:
+				d[k][int(folder)] = 1
+				name = image[9:11]
+				if name != '29':
+					continue
+				img = cv2.imread('GTSRB/Final_Training/Images/' + folder + '/' + image, 1)
+				img = cv2.resize(img, (32, 32))
+				# print(img.shape)
+				data = [int(k)]
+				for i in range(0, 3):
+					data += list(img[:, :, i].reshape(1, -1).squeeze())
+				dataset.append(data)
+			k+=1
+			print("hello", end=' ')
+			print(k)
+
 
 dataset = np.array(dataset, dtype = np.uint8)
-print dataset.shape
+print (dataset.shape)
+# for i in range(43):
+# 	for j in range(43):
+# 		print(d[i][j], end=" ")
+# 	print("\n")
 shuffle = np.arange(len(dataset))
 np.random.shuffle(shuffle)
 dataset	= dataset[shuffle]
-np.savetxt("dataset.txt", dataset, fmt = '%i', delimiter = ' ', newline = '\n')
+np.savetxt("new_dataset.txt", dataset, fmt = '%i', delimiter = ' ', newline = '\n')
 
 ## For testing the saved dataset
 # file = open("dataset.txt", 'r')
